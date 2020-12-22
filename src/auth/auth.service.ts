f168f06c6acee6b4ba93612c0b6560c6c98fe155
Jeremie Valdenaire <jeremie.valdenaire@epitech.eu>
2020-12-22 18:05:28 +0100

    Register and login DONE. Starting to add guards

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+// import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { from, Observable, of } from 'rxjs';
+import { User } from '../users/users.model';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly JwtService: JwtService) {}
+
+  generateJWT(user: User): Observable<string> {
+    return from(this.JwtService.signAsync({ user }));
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 12);
+  }
+
+  comparePasswords(newPassword: string, passwordHash: string): Observable<any> {
+    return of<any | boolean>(bcrypt.compare(newPassword, passwordHash));
+  }
+}
